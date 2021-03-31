dd525987de671d04580994ff3b5cd60ceb90bd15
Valentin Wettstein <valentin.wettstein@santismail.ch>
2021-03-31 14:13:43 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
 import Navigation from './Navigation.js';
 import Footer from './Footer.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-          <Navigation />
-      </header>
-      
-      <footer>
-          <Footer />
-      </footer>
+    <Router>
+      <div className="App">
+          <header>
+              <Navigation />
+          </header>
 
-    </div>
+          <Route exact path="/">
+            <p>Home</p>
+          </Route>
+
+          <Route path="/about">
+            <p>About</p>
+          </Route>
+
+          <Route path="/contact">
+            <p>Contact</p>
+          </Route>
+
+          <footer>
+              <Footer />
+          </footer>
+      </div>
+    </Router>
   );
 }
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export const Navigation = () => {
     return (
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     )
